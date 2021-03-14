2cab451041834d7022e2b53435e3ad440c99931c
KailanySousa <kailany-sousa2010@hotmail.com>
2021-03-14 13:02:38 -0300

    iniciando projeto

---
+++ crud/lib/data/dummy_users.dart
@@ -0,0 +1,22 @@
+import 'package:crud/models/user.dart';
+
+const DUMMY_USERS = {
+  '1': const User(
+      id: '1',
+      name: 'Kailany',
+      email: 'kailany@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2016/08/28/13/12/secondlife-1625903_960_720.jpg'),
+  '2': const User(
+      id: '1',
+      name: 'Ayla',
+      email: 'ayla@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/02/25/10/04/woman-6048694_960_720.png'),
+  '3': const User(
+      id: '1',
+      name: 'Maria',
+      email: 'maria@gmail.com',
+      avatarUrl:
+          'https://cdn.pixabay.com/photo/2021/03/01/08/56/woman-6059190__340.png')
+};
